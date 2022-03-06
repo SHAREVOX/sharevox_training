c32ce43b5b11e3642156f20dc2619314b495cbca
y-chan <y-chan@y-chan.dev>
2022-03-06 18:29:21 +0900

    add optimizer

---
+++ modules/optimizer.py
@@ -0,0 +1,73 @@
+import torch
+import numpy as np
+
+from dataset import TrainConfig
+from modules.fastspeech2 import PitchAndDurationPredictor, MelSpectrogramDecoder, ModelConfig
+
+
+class ScheduledOptim:
+    """ A simple wrapper class for learning rate scheduling """
+
+    def __init__(
+        self,
+        variance_model: PitchAndDurationPredictor,
+        decoder_model: MelSpectrogramDecoder,
+        train_config: TrainConfig,
+        model_config: ModelConfig,
+        current_step: int
+    ):
+
+        self._variance_optimizer = torch.optim.Adam(
+            variance_model.parameters(),
+            betas=train_config["optimizer"]["betas"],
+            eps=train_config["optimizer"]["eps"],
+            weight_decay=train_config["optimizer"]["weight_decay"],
+        )
+        self._decoder_optimizer = torch.optim.Adam(
+            decoder_model.parameters(),
+            betas=train_config["optimizer"]["betas"],
+            eps=train_config["optimizer"]["eps"],
+            weight_decay=train_config["optimizer"]["weight_decay"],
+        )
+
+        self.n_warmup_steps = train_config["optimizer"]["warm_up_step"]
+        self.anneal_steps = train_config["optimizer"]["anneal_steps"]
+        self.anneal_rate = train_config["optimizer"]["anneal_rate"]
+        self.current_step = current_step
+        self.init_lr = np.power(model_config["encoder"]["hidden"], -0.5)
+
+    def step_and_update_lr(self) -> None:
+        self._update_learning_rate()
+        self._variance_optimizer.step()
+        self._decoder_optimizer.step()
+
+    def zero_grad(self) -> None:
+        # print(self.init_lr)
+        self._variance_optimizer.zero_grad()
+        self._decoder_optimizer.zero_grad()
+
+    def load_state_dict(self, path: dict) -> None:
+        self._variance_optimizer.load_state_dict(path["variance"])
+        self._decoder_optimizer.load_state_dict(path["decoder"])
+
+    def _get_lr_scale(self) -> None:
+        lr = np.min(
+            [
+                np.power(self.current_step, -0.5),
+                np.power(self.n_warmup_steps, -1.5) * self.current_step,
+            ]
+        )
+        for s in self.anneal_steps:
+            if self.current_step > s:
+                lr = lr * self.anneal_rate
+        return lr
+
+    def _update_learning_rate(self) -> None:
+        """ Learning rate scheduling per step """
+        self.current_step += 1
+        lr = self.init_lr * self._get_lr_scale()
+
+        for param_group in self._variance_optimizer.param_groups:
+            param_group["lr"] = lr
+        for param_group in self._decoder_optimizer.param_groups:
+            param_group["lr"] = lr
