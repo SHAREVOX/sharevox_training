2707d3075c19154e7135318975e06f3dd8ed6457
y-chan <y-chan@y-chan.dev>
2022-03-06 16:57:39 +0900

    add plot function

---
+++ utils/plot.py
@@ -0,0 +1,37 @@
+from typing import List, Tuple
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def plot_mel(
+    data: List[Tuple[np.ndarray, np.ndarray, np.ndarray]], titles: List[str]
+) -> plt.Figure:
+    plot_data: Tuple[plt.Figure, plt.Axes] = plt.subplots(len(data), 1, squeeze=False)
+    fig, axes = plot_data
+    if titles is None:
+        titles = [None for i in range(len(data))]
+
+    def add_axis(fig: plt.Figure, old_ax: plt.Axes) -> plt.Axes:
+        ax = fig.add_axes(old_ax.get_position(), anchor="W")
+        ax.set_facecolor("None")
+        return ax
+
+    for i in range(len(data)):
+        mel, pitch, energy = data[i]
+        axes[i][0].imshow(mel, origin="lower")
+        axes[i][0].set_aspect(2.5, adjustable="box")
+        axes[i][0].set_ylim(0, mel.shape[0])
+        axes[i][0].set_title(titles[i], fontsize="medium")
+        axes[i][0].tick_params(labelsize="x-small", left=False, labelleft=False)
+        axes[i][0].set_anchor("W")
+
+        ax1 = add_axis(fig, axes[i][0])
+        ax1.plot(pitch, color="tomato")
+        ax1.set_xlim(0, mel.shape[1])
+        ax1.set_ylabel("F0", color="tomato")
+        ax1.tick_params(
+            labelsize="x-small", colors="tomato", bottom=False, labelbottom=False
+        )
+
+    return fig
