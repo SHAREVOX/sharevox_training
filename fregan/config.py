7fe89b3112a8138853bfdfd6b69eb15221c5d56d
y-chan <y-chan@y-chan.dev>
2022-03-07 12:14:43 +0900

    add fregan

---
+++ fregan/config.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Config:
+    def __init__(self):
+        self.resblock: str = "1"
+        self.num_gpus: int = 0
+        self.batch_size: int = 16
+        self.learning_rate: float = 0.0002
+        self.adam_b1: float = 0.8
+        self.adam_b2: float = 0.99
+        self.lr_decay: float = 0.999
+        self.seed: int = 1234
+        self.disc_start_step: int = 0
+
+        self.upsample_rates: List[int] = [8, 4, 2, 2, 2]
+        self.upsample_kernel_sizes: List[int] = [16, 8, 4, 4, 4]
+        self.upsample_initial_channel: int = 512
+        self.resblock_kernel_sizes: List[int] = [3, 7, 11]
+        self.resblock_dilation_sizes: List[List[int]] = [[1, 3, 5, 7], [1, 3, 5, 7], [1, 3, 5, 7]]
+
+        self.segment_size: int = 8192
+        self.num_mels: int = 80
+        self.num_freq: int = 1025
+        self.n_fft: int = 1024
+        self.hop_size: int = 256
+        self.win_size: int = 1024
+
+        self.fmin: int = 0
+        self.fmax: int = 8000
+        self.fmax_for_loss: None = None
+
+        self.num_workers: int = 4
