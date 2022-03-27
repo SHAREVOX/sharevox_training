3f43d8bec75188754efe8fbe67cbd7a6cf6bee93
y-chan <y-chan@y-chan.dev>
2022-03-28 03:12:02 +0900

    add onnx export and synthesis

---
+++ onnx_export.py
@@ -0,0 +1,146 @@
+import argparse
+from typing import Optional
+
+import numpy as np
+import torch
+import yaml
+
+from torch import nn, Tensor, LongTensor
+
+from fregan import Generator
+from modules.fastspeech2 import MelSpectrogramDecoder, PitchAndDurationPredictor, FeatureEmbedder
+from text import phoneme_to_id, accent_to_id
+from utils.model import Config, get_model, get_vocoder
+
+from torch.onnx.symbolic_registry import _onnx_main_opset
+
+OPSET = _onnx_main_opset
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
+
+class Variance(nn.Module):
+    def __init__(self, config: Config, variance_model: PitchAndDurationPredictor):
+        super(Variance, self).__init__()
+
+        self.sampling_rate = config["preprocess"]["audio"]["sampling_rate"]
+        self.hop_length = config["preprocess"]["stft"]["hop_length"]
+        self.variance_model = variance_model
+
+    def forward(
+        self,
+        phonemes: Tensor,
+        accents: Tensor,
+        speakers: Tensor,
+    ):
+        log_pitches, log_durations = self.variance_model(phonemes, accents, speakers)
+        durations = torch.clamp((torch.exp(log_durations) - 1) / (self.sampling_rate / self.hop_length), min=0.01)
+        return log_pitches, durations
+
+
+class Embedder(nn.Module):
+    def __init__(self, config: Config, embedder_model: FeatureEmbedder):
+        super(Embedder, self).__init__()
+
+        self.sampling_rate = config["preprocess"]["audio"]["sampling_rate"]
+        self.hop_length = config["preprocess"]["stft"]["hop_length"]
+        self.embedder_model = embedder_model
+
+    def forward(
+        self,
+        phonemes: Tensor,
+        pitches: Tensor,
+        speakers: Tensor,
+    ):
+        pitches = torch.exp(pitches)
+        feature_embedded = self.embedder_model(phonemes, pitches, speakers)
+        return feature_embedded
+
+
+class Decoder(nn.Module):
+    def __init__(self, config: Config, decoder: MelSpectrogramDecoder, fregan: Generator):
+        super(Decoder, self).__init__()
+
+        self.max_wav_value = config["preprocess"]["audio"]["max_wav_value"]
+        self.decoder = decoder
+        self.fregan = fregan
+
+    def forward(self, length_regulated_tensor: Tensor) -> Tensor:
+        _, postnet_outputs = self.decoder(length_regulated_tensor)
+        wavs = self.fregan(postnet_outputs[0].transpose(0, 1).unsqueeze(0)).squeeze(1) * self.max_wav_value
+        wavs = wavs.to(torch.int16)
+        return wavs
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--restore_step", type=int, default=0)
+    parser.add_argument("--speaker_num", type=int, default=1)
+    parser.add_argument(
+        "-c", "--config", type=str, required=True, help="path to config yaml"
+    )
+    args = parser.parse_args()
+    config: Config = yaml.load(
+        open(args.config, "r"), Loader=yaml.FullLoader
+    )
+
+    variance_model, embedder_model, decoder_model, _ = get_model(args.restore_step, config, device, args.speaker_num, False)
+    fregan_model = get_vocoder(device)
+    variance_model = Variance(config, variance_model)
+    embedder_model = Embedder(config, embedder_model)
+    decoder_model = Decoder(config, decoder_model, fregan_model)
+    decoder_model.eval()
+    decoder_model.requires_grad_ = False
+
+    phonemes = torch.from_numpy(np.array([[phoneme_to_id[p] for p in "k o N n i ch i w a".split(" ")]])).to(dtype=torch.int, device=device)
+    accents = torch.from_numpy(np.array([[accent_to_id[a] for a in "_ [ _ _ _ _ _ _ #".split(" ")]])).to(dtype=torch.int, device=device)
+    speakers = torch.from_numpy(np.array([0])).to(dtype=torch.int, device=device)
+    torch.onnx.export(
+        variance_model,
+        (
+            phonemes, accents, speakers
+        ),
+        "variance_model.onnx",
+        input_names=["phonemes", "accents", "speakers"],
+        output_names=["pitches", "durations"],
+        dynamic_axes={
+            "phonemes": {1: "inLength"},
+            "accents": {1: "inLength"},
+            "pitches": {1: "outLength"},
+            "durations": {1: "outLength"}
+        },
+        opset_version=OPSET,
+    )
+
+    pitches = torch.from_numpy(np.array([[5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5]])).to(dtype=torch.float, device=device)
+    embedber_input = (phonemes, pitches, speakers)
+    torch.onnx.export(
+        embedder_model,
+        embedber_input,
+        "embedder_model.onnx",
+        input_names=["phonemes", "pitches", "speakers"],
+        output_names=["feature_embedded"],
+        dynamic_axes={
+            "phonemes": {1: "inLength"},
+            "pitches": {1: "inLength"},
+            "feature_embedded": {1: "outLength"},
+        },
+        opset_version=OPSET,
+    )
+    embedded_tensor = embedder_model(*embedber_input)
+
+    torch.onnx.export(
+        decoder_model,
+        (
+            embedded_tensor,
+        ),
+        "decoder_model.onnx",
+        input_names=["length_regulated_tensor"],
+        output_names=["wav"],
+        dynamic_axes={
+            "length_regulated_tensor": {1: "length"},
+            "wav": {1: "outLength"},
+        },
+        opset_version=OPSET,
+    )
---
+++ onnx_synthesize.py
@@ -0,0 +1,71 @@
+import argparse
+
+import numpy
+from scipy.io import wavfile
+import numpy as np
+from pyopenjtalk import extract_fullcontext
+
+from text import extract_phoneme_and_accents
+from text import phoneme_to_id, accent_to_id
+import onnxruntime
+
+
+def preprocess_japanese(text):
+    full_context_labels = extract_fullcontext(text)
+    phonemes, accents = extract_phoneme_and_accents(full_context_labels)
+    phonemes_seq = np.array([phoneme_to_id[phoneme] for phoneme in phonemes])
+    accents_seq = np.array([accent_to_id[accent] for accent in accents])
+
+    return phonemes_seq, accents_seq
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--text",
+        type=str,
+        default=None,
+        help="raw text to synthesize, for single-sentence mode only",
+    )
+    parser.add_argument(
+        "--speaker_id",
+        type=int,
+        default=0,
+        help="speaker ID for multi-speaker synthesis, for single-sentence mode only",
+    )
+    args = parser.parse_args()
+
+    # Check source texts
+    assert args.text is not None
+
+    # Preprocess texts
+    ids = raw_texts = [args.text[:100]]
+    speakers = np.array([args.speaker_id], dtype=np.int32)
+    accents = None
+    phonemes, accents = [np.array([out]).astype(dtype=np.int32) for out in preprocess_japanese(args.text)]
+
+    variance_session = onnxruntime.InferenceSession("variance_model.onnx", providers=['CUDAExecutionProvider'])
+    embedder_session = onnxruntime.InferenceSession("embedder_model.onnx", providers=['CUDAExecutionProvider'])
+    decoder_session = onnxruntime.InferenceSession("decoder_model.onnx", providers=['CUDAExecutionProvider'])
+
+    pitches, durations = variance_session.run(["pitches", "durations"], {
+        "phonemes": phonemes,
+        "accents": accents,
+        "speakers": speakers,
+    })
+
+    feature_embedded = embedder_session.run(["feature_embedded"], {
+        "phonemes": phonemes,
+        "pitches": pitches[0].T,
+        "speakers": speakers,
+    })[0]
+
+    durations = (durations[0].T * (48000 / 256)).astype(dtype=np.int64)
+    length_regulated_tensor = numpy.repeat(feature_embedded, durations[0], axis=1)
+
+    wav = decoder_session.run(["wav"], {
+        "length_regulated_tensor": length_regulated_tensor,
+    })[0]
+
+    wavfile.write("test.wav", 48000, wav[0])
