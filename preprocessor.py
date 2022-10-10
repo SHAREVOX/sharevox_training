3d7f7eb29bc0ead05a5a12bbca993e62d344fecb
Yuto Ashida <y-chan@y-chan.dev>
2022-10-10 14:55:55 +0900

    comment out interp

--- preprocessor.py
+++ preprocessor.py
@@ -216,14 +216,14 @@ class Preprocessor:
         if np.sum(pitch != 0) <= 1:
             raise RuntimeError()
 
-        nonzero_ids = np.where(pitch != 0)[0]
-        interp_fn = interp1d(
-            nonzero_ids,
-            pitch[nonzero_ids],
-            fill_value=(pitch[nonzero_ids[0]], pitch[nonzero_ids[-1]]),
-            bounds_error=False,
-        )
-        pitch: np.ndarray = interp_fn(np.arange(0, len(pitch)))
+        # nonzero_ids = np.where(pitch != 0)[0]
+        # interp_fn = interp1d(
+        #     nonzero_ids,
+        #     pitch[nonzero_ids],
+        #     fill_value=(pitch[nonzero_ids[0]], pitch[nonzero_ids[-1]]),
+        #     bounds_error=False,
+        # )
+        # pitch: np.ndarray = interp_fn(np.arange(0, len(pitch)))
         pitch = pitch[:duration]
 
         # Compute mel-scale spectrogram
